ecd27c351b1c54fca3a53b8af19a05d7c40a344a
Renê <renejr.arraes286@gmail.com>
2020-12-12 10:27:40 -0300

    crete product service

---
+++ src/app/models/users/signup-user.ts
@@ -0,0 +1,6 @@
+export interface SignUpUser{
+    name: string;
+    cpf: string;
+    email: string;
+    password: string;
+}
---
+++ src/app/services/sign-service.service.ts
@@ -0,0 +1,43 @@
+import { SignUpUser } from './../models/users/signup-user';
+import { LoginUser } from './../models/users/login-user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignServiceService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  
+  apiUrl = "renejr-ecommerce.herokuapp.com";
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(user: LoginUser){
+    return this.httpClient.post<any>(
+      this.apiUrl + '/login',
+      JSON.stringify(user),
+      this.httpOptions,
+    ) 
+  }
+
+  signUpSeller(seller: SignUpUser){
+    return this.httpClient.post<any>(
+      this.apiUrl + '/create/seller',
+      JSON.stringify(seller),
+      this.httpOptions,
+    ) 
+  }
+  signUpClient(client: SignUpUser){
+    return this.httpClient.post<any>(
+      this.apiUrl + '/create/client',
+      JSON.stringify(client),
+      this.httpOptions,
+    ) 
+  }
+}
