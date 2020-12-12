460dcb4e2d0e3e875f095014371b8aa4138a98f5
Renê <renejr.arraes286@gmail.com>
2020-12-12 10:08:59 -0300

    implement confirmation dialog when you buy product from wishlist

--- src/app/client-page/wishlist-page/wishlist-page.component.ts
+++ src/app/client-page/wishlist-page/wishlist-page.component.ts
@@ -1,5 +1,7 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfirmationDialogComponent } from 'src/app/dialogs/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-wishlist-page',
@@ -8,10 +10,35 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class WishlistPageComponent implements OnInit {
 
-  constructor(private _snackBar: MatSnackBar) { }
+  constructor(private _snackBar: MatSnackBar, private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+
+  openBuyDialog(productName: string){
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+
+      if(result){
+        this.buyProduct(productName, 'Dismiss');
+      }
+    });
+  
+  }
+
+    buyProduct(productName: string, action: string) {
+      this._snackBar.open(
+        'You have bought the product |' + productName + '|' + 
+        " We're sending a confirmation email to your inbox",
+        action,
+        
+  
+        {
+          duration: 5000,
+        }
+      );
+    }
 
   addProductInYourWishlist(){
     console.log('deu certo');
